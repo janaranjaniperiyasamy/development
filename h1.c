67834d2a39b9a72c3de27d764c50bc38dfbedf33
janaranjaniperiyasamy <46353547+janaranjaniperiyasamy@users.noreply.github.com>
2019-04-01 16:00:41 +0530

    h1.c

---
+++ h1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10000],i,j,n,k=0,b[1000],t;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]!='*')
+        {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]!='*')
+            {
+            if(a[i]==a[j])
+            {
+                b[k]=a[i];
+                k++;
+                a[j]='*';
+                 a[i]='*';
+        
+            }
+            }
+        }
+       
+       
+        }
+    }
+for(i=0;i<k;i++)
+{
+for(j=i;j<k;j++)
+{
+    if(b[i]>b[j])
+    {
+        t=b[i];
+        b[i]=b[j];
+        b[j]=t;
+    }
+}
+}
+for(i=0;i<k;i++)
+{
+    printf("%d ",b[i]);
+}
+    return 0;
+}
